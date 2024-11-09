dab3ad05e9716c397133bdec3acfcca8fa955ca3
JulianoRR <jubaa@live.com>
2024-11-09 09:45:02 -0300

    primary commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/product.model.js')
 const ProductRoute = require('./routes/product.route.js')
 const app = express()
+require('dotenv').config()
 
 //midlleware
 app.use(express.json())
@@ -15,15 +15,12 @@ app.get('/', (req, res) => {
     res.send('Hello from Node API update')
 })
 
+//database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => { console.log('Connected to database') })
+    .catch(() => { console.log("Connection Failed!") })
 
-mongoose.connect("mongodb+srv://julianoramboribas:mongo2030@node-api.leznjwy.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Node-API")
-    .then(() => {
-        console.log('Connected to database')
-    })
-    .catch(() => {
-        console.log("Connection Failed!")
-    })
-
+//server
 app.listen(3000, () => {
     console.log('Server is running : http://localhost:3000')
 })
